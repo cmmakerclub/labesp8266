d379cea85d251c31cf1af7b2226a8b01a84d39f4
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-07-08 21:49:05 +0700

    no message

--- DemoNETPIE/DHT_Freeboard/DHT_Freeboard.ino
+++ DemoNETPIE/DHT_Freeboard/DHT_Freeboard.ino
@@ -1,31 +1,29 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
-#include <MicroGear.h>
 #include <ESP8266WiFi.h>
+#include <MicroGear.h>  // v 1.1.7
 #include <ESPert.h>
 #include "DHT.h"
 
-const char* ssid     = "@ESPertAP_001";  // Change your ssid wifi : ESPERT-002
+const char* ssid     = "ESPERT-002";  // Change your ssid wifi : @ESPertAP_001 or ESPERT-002
 const char* password = "espertap";  // Change your password wifi : espertap
 
-// NETPIE.io : man_LightControl
+// NETPIE.io : man_DHT
 #define APPID   "HelloCMMC"             // Change your appID
-#define KEY     "BZYTbAa9ItnMyeW"       // Change your Key
-#define SECRET  "h4aeKNOFIlSatTS8ADNk3Ft3O" // Change your SECRET
-#define ALIAS   "smartuniversity"       // Change your name
+#define KEY     "3CGFOspur1y1hVF"       // Change your Key
+#define SECRET  "T1eUhstPrXG86hyFtNTuFmpD0" // Change your SECRET
+#define ALIAS   "DHTFreeboard"       // Change your name
 
-#define LEDPin 16
+#define LEDPin 2
 #define DHTPIN 12
 #define DHTTYPE DHT22
-
 
 ESPert espert;
 WiFiClient client;
 MicroGear microgear(client);
 DHT dht(DHTPIN, DHTTYPE);
 
-void init_hardware();
-void init_wifi();
+//void init_hardware();
+//void init_wifi();
 
 /******************* microgear loop ***********************************/
 void onFoundgear(char *attribute, uint8_t* msg, unsigned int msglen) {
@@ -51,33 +49,50 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
   Serial.print("Incoming message --> ");
   Serial.print(topic);
   Serial.print(" : ");
-  char strState[msglen];
+
+  char Str_msg[msglen];
   for (int i = 0; i < msglen; i++)
   {
-    strState[i] = (char)msg[i];
+    Str_msg[i] = (char)msg[i];
     Serial.print((char)msg[i]);
   }
 
   Serial.println();
+
+  String msgIN = String(Str_msg).substring(0, msglen);
+
+  Serial.print("Topic is ");
+  Serial.println(topic);
+
+  if (msgIN == "ON")
+  {
+    digitalWrite(LEDPin, LOW);
+  }
+  else if (msgIN == "OFF")
+  {
+    digitalWrite(LEDPin, HIGH);
+  }
 }
 
 void init_hardware() {
+  espert.init();
+  espert.oled.init();
+  delay(2000);
+
+  pinMode(LEDPin, OUTPUT);
+  digitalWrite(LEDPin, LOW);
+
+  dht.begin();
+}
+
+void init_wifi() {
   microgear.on(MESSAGE, onMsghandler);
   microgear.on(PRESENT, onFoundgear);
   microgear.on(ABSENT, onLostgear);
   microgear.on(CONNECTED, onConnected);
 
   Serial.begin(115200);
-  espert.init();
-  espert.oled.init();
-  delay(2000);
 
-  dht.begin();
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
-}
-
-void init_wifi() {
   if (WiFi.begin(ssid, password)) {
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -87,15 +102,19 @@ void init_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-  microgear.resetToken();
+
+  Serial.println("connecting netpie.io...");
+  //microgear.resetToken();
   microgear.init(KEY, SECRET, ALIAS);
   microgear.connect(APPID);
+  Serial.println("netpie.io connected.");
 }
 
 /************************* loop ***********************************/
 void setup() {
-  init_hardware();
   init_wifi();
+  init_hardware();
+  Serial.println("Init done...");
 }
 
 void loop() {
@@ -104,37 +123,31 @@ void loop() {
     microgear.loop();
 
     // อ่านค่าจากเซ็นเซอร์ DHt22
-    float tempread = dht.readTemperature();
-    float humidread = dht.readHumidity();
-
-    // แปลงค่า float ให้เป็น String
-    char buffer[50];
-    String temp = dtostrf(tempread, 4, 2, buffer);
-    String humid = dtostrf(humidread, 4, 2, buffer);
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
 
     // ส่งค่าไปยัง netpie
-    microgear.chat("smartuniversity/Temperature", temp.c_str());
-    microgear.chat("smartuniversity/Humidity", humid.c_str());
+    microgear.chat("DHTFreeboard/Temperature", String(t));
+    microgear.chat("DHTFreeboard/Humidity", String(h));
 
     espert.oled.clear();
     espert.oled.setTextSize(1);
     espert.oled.setTextColor(ESPERT_WHITE);
     espert.oled.setCursor(0, 24);
 
-    espert.oled.println("Hello NETPIE");
+    espert.oled.println("   Hello NETPIE");
     espert.oled.print("Temperature = ");
     espert.oled.println(dht.readTemperature());
     espert.oled.print("Humidity = ");
     espert.oled.println(dht.readHumidity());
     espert.oled.update();
 
-    Serial.println("connect...");
     Serial.print("Temperature = ");
-    Serial.print(tempread);
+    Serial.print(t);
     Serial.print("\t");
     Serial.print("Humidity = ");
-    Serial.println(humidread);
-    delay(1000);
+    Serial.println(h);
+    delay(500);
   }
   else
   {
